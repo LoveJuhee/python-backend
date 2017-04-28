af940dd74c99d03a14edb7ac5425065e9de67ca7
hyounwoo.ko <lj1212.work@gmail.com>
2017-04-28 12:25:23 +0900

    client 계정관리 기능 추가 및 클라이언트 접속 종료시 알림

--- backend/consumers.py
+++ backend/consumers.py
@@ -9,6 +9,8 @@ from .models import Center
 
 log = CustomLogger.__call__().get_logger()
 
+accounts = []
+
 
 def __get_group(label, channel_layer):
     return Group('chat-' + label, channel_layer=channel_layer)
@@ -17,36 +19,38 @@ def __get_group(label, channel_layer):
 @channel_session
 def ws_connect(message):
     try:
-        prefix, label = message['path'].strip('/').split('/')
+        prefix, label, id = message['path'].strip('/').split('/')
         if prefix != 'chat':
             log.debug('invalid ws path=%s', message['path'])
             return
-        room = Center.objects.get(label=label)
+        center = Center.objects.get(label=label)
+        account = {'id': id, 'key': str(message.reply_channel)}
+        accounts.append(account)
     except ValueError as e:
         log.debug('invalid ws path=%s', message['path'])
         return
 
-    log.debug('chat connect room=%s client=%s:%s path=%s reply_channel=%s',
-              room.label, message['client'][0], message['client'][1],
+    log.debug('chat connect center=%s client=%s:%s path=%s reply_channel=%s',
+              center.label, message['client'][0], message['client'][1],
               message['path'], message.reply_channel)
 
     message.reply_channel.send({"accept": True})
     __get_group(label, message.channel_layer).add(message.reply_channel)
 
-    message.channel_session['room'] = room.label
+    message.channel_session['center'] = center.label
 
 
 @channel_session
 def ws_receive(message):
     try:
-        label = message.channel_session['room']
-        room = Center.objects.get(label=label)
-        log.debug('label=%s, room=%s', label, room)
+        label = message.channel_session['center']
+        center = Center.objects.get(label=label)
+        log.debug('label:%s, center.name:%s', label, center.name)
     except KeyError:
-        log.debug('no room in channel_session')
+        log.debug('no center in channel_session')
         return
     except Center.DoesNotExist:
-        log.debug('recieved message, but room does not exist label=%s', label)
+        log.debug('recieved message, but center does not exist label=%s', label)
         return
 
     try:
@@ -60,9 +64,9 @@ def ws_receive(message):
         return
 
     if data:
-        log.debug('chat message room=%s handle=%s message=%s',
-                  room.label, data['handle'], data['message'])
-        m = room.messages.create(**data)
+        log.debug('chat message center=%s handle=%s message=%s',
+                  center.label, data['handle'], data['message'])
+        m = center.messages.create(**data)
 
         group = __get_group(label, message.channel_layer)
         group.send({'text': json.dumps(m.as_dict())})
@@ -71,9 +75,28 @@ def ws_receive(message):
 @channel_session
 def ws_disconnect(message):
     try:
-        label = message.channel_session['room']
-        # room = Center.objects.get(label=label)
+        label = message.channel_session['center']
+        center = Center.objects.get(label=label)
         group = __get_group(label, message.channel_layer)
         group.discard(message.reply_channel)
+
+        log.debug('label:%s, channel_layer:%s, center.name:%s',
+                  label,
+                  message.channel_layer,
+                  center.name)
+
+        key = str(message.reply_channel)
+        for tmp in accounts:
+            if tmp['key'] == key:
+                account = tmp
+                break
+        accounts.remove(account)
+        log.debug('account size: %d', len(accounts))
+
+        data = {
+            'message': account['id'] + ' is disconnected.'
+        }
+
+        group.send({'text': json.dumps(data)})
     except (KeyError, Center.DoesNotExist):
         pass
